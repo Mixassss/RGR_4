ba315cd7dbb8eb7d698b39dc2bf295d3f995c1f9
Mixassss <163171930+Mixassss@users.noreply.github.com>
2025-05-21 12:51:21 +0700

    Update App.js

--- relay_audit/src/App.js
+++ relay_audit/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Profile from "./pages/Profile";
 import Register from "./pages/Register";
 import Upload from "./pages/Upload";
 import Report from "./pages/Report";
@@ -14,6 +15,7 @@ const App = () => (
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>} />
+            <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
             <Route path="/upload" element={<PrivateRoute><Upload /></PrivateRoute>} />
             <Route path="/report" element={<PrivateRoute><Report /></PrivateRoute>} />
             <Route path="/details/:fileId" element={<PrivateRoute><Details /></PrivateRoute>} />
